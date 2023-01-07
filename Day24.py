b99671458d4a2c0ec419c30018089b9333544df1
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-01-07 22:53:42 +0530

    fstring

---
+++ Day24.py
@@ -0,0 +1,8 @@
+price = 5.232425
+
+# so here 2f is for only 2 point from decimal
+print(f"hey price if {price:.2f}")
+
+
+# if we want to show curly braces in my f string so we use double curly braces
+print(f"hey ram pal {{name}}")
